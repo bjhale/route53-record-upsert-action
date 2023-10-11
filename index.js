207b7c6c2bf35085b526869213ee81f43cad5e14
billy.hale <billy.hale@gce.com>
2023-10-10 19:14:20 -0700

    Initial Commit

---
+++ index.js
@@ -0,0 +1,38 @@
+import { Route53Client, ChangeResourceRecordSetsCommand } from '@aws-sdk/client-route-53';
+import core from '@actions/core';
+
+const client = new Route53Client();
+const zoneId = core.getInput('route53_zone_id');
+const name = core.getInput('record_name');
+const type = core.getInput('record_type');
+const value = core.getInput('record_value');
+const ttl = core.getInput('record_ttl');
+
+try {
+  const input = {
+    HostedZoneId: zoneId,
+    ChangeBatch: {
+      Changes: [
+        {
+          Action: 'UPSERT',
+          Name: name,
+          Type: type,
+          TTL: ttl,
+          ResourceRecords: [
+            {
+              Value: value
+            }
+          ]
+        }
+      ]
+    }
+  }
+
+  const command = new ChangeResourceRecordSetsCommand(input);
+  const response = await client.send(command);
+
+  console.log(response);
+} catch (error) {
+  core.setFailed(error.message);
+}
+
